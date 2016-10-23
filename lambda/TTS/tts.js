36f079c80dea336f39b1b88d97bcb5336fc93ffd
yeongjinc <yeongjinc@gmail.com>
2016-10-24 00:13:06 +0900

    clean code

--- lambda/TTS/tts.js
+++ lambda/TTS/tts.js
@@ -20,37 +20,55 @@ const aws = require('aws-sdk');
 const fs = require('fs');
 const cp = require('child_process');
 
-function uploadS3(tempName, fileName, filePath, callback) {
+const DEFAULT_PATH = '/tmp/';
+
+function uploadAndClean(tempName, fileName, callback) {
 	var s3 = new aws.S3();
-	var file = fs.createReadStream(filePath);
+	var file = fs.createReadStream(DEFAULT_PATH + fileName);
 	var s3param = {Bucket: 'koreantts', Key: fileName, Body: file};
 	s3.upload(s3param, function(err, data) {
-		if (err) console.log(err, err.stack);
-		else console.log(data);
+		if (err) console.log('S3 upload error : ' + err, err.stack);
+		else console.log('S3 upload succeed : ' + data);
 
-		cp.exec('rm -rf /tmp/' + tempName + ' ' + filePath,
+		cp.exec('rm -rf ' + DEFAULT_PATH  + tempName + ' ' + DEFAULT_PATH + fileName,
 			function(error, stdout, stderr) {
 				if(error)
-					console.log('deleting temp error');
+					console.log('deleting temp error : ' + error);
 				else
-					console.log('deleting temp ok');
+					console.log('deleting temp succeed');
 				callback(null, 'https://s3.amazonaws.com/koreantts/' + fileName);
 			}
 		);
 	});
 }
 
+function convertMP3(tempName, fileName, callbackAfterUpload) {
+	cp.exec(
+		DEFAULT_PATH + 'ffmpeg -analyzeduration 10000000 -probesize 100000000 -i ' + DEFAULT_PATH
+		+ tempName + ' -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ' + DEFAULT_PATH + fileName,
+		function(error, stdout, stderr) {
+			if(error) {
+				console.log('executing ffmpeg error : RETRY');
+				convertMP3(tempName, fileName, callbackAfterUpload);
+			} else {
+				console.log('executing ffmpeg');
+				uploadAndClean(tempName, fileName, callbackAfterUpload);
+			}
+		}
+	);
+}
+
 exports.handler = (event, context, callback) => {
-    var options = {
-        hostname: 'openapi.naver.com',
-        port: 443,
-        path: '/v1/voice/tts.bin',
-        method: 'post',
-        headers: {
+	var options = {
+		hostname: 'openapi.naver.com',
+		port: 443,
+		path: '/v1/voice/tts.bin',
+		method: 'post',
+		headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            /*'X-Naver-Client-Id': '9NV5mB_ADv_99e3t_oey',
-            'X-Naver-Client-Secret': 'jlVolLMkVy',*/
-            'X-Naver-Client-Id': '2pwrCYsr_uLVHKaNajHR',
+            //'X-Naver-Client-Id': '9NV5mB_ADv_99e3t_oey', 		// yeongjin
+            //'X-Naver-Client-Secret': 'jlVolLMkVy',
+            'X-Naver-Client-Id': '2pwrCYsr_uLVHKaNajHR', 		// sanghoon
             'X-Naver-Client-Secret': 'na0qFADoLh',
         }
     };
@@ -61,70 +79,26 @@ exports.handler = (event, context, callback) => {
 	});
 
 	var tempName = new Date().getTime() + '.mp3';
-	tempName = 'testtesttest.mp3'
 	cp.exec(
-		'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg; ls -l /tmp/ffmpeg; rm -rf /tmp/' + tempName,
+		'cp /var/task/ffmpeg ' + DEFAULT_PATH + '; chmod 755 ' + DEFAULT_PATH + 'ffmpeg; '
+		+ 'ls -l ' + DEFAULT_PATH + 'ffmpeg; rm -rf ' + DEFAULT_PATH + tempName,
 		function(error, stdout, stderr) {
 			if(error) {
-				console.log('loading ffmpeg error');
+				console.log('loading ffmpeg error : ' + error);
 			} else {
-				console.log('loading ffmpeg');
-				console.log('stdout : ' + stdout);
-				console.log('stderr : ' + stderr);
+				console.log('loading ffmpeg succeed');
+				//console.log('stdout : ' + stdout);
+				//console.log('stderr : ' + stderr);
 				const req = https.request(options, (res) => {
 					console.log('TTS api returned');
-					var tempFile = fs.createWriteStream('/tmp/' + tempName);
+					var tempFile = fs.createWriteStream(DEFAULT_PATH + tempName);
 					res.pipe(tempFile);
 
 					var fileName = new Date().getTime() + '.mp3';
 					if(event.fileName)
 						fileName = event.fileName;
-					var filePath = "/tmp/" + fileName;
 
-					cp.exec(
-						'/tmp/ffmpeg -analyzeduration 10000000 -probesize 100000000 -i /tmp/' + tempName + ' -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ' + filePath,
-						function(error, stdout, stderr) {
-							if(error) {
-								console.log('executing ffmpeg error 1');
-								cp.exec(
-						'/tmp/ffmpeg -analyzeduration 10000000 -probesize 100000000 -i /tmp/' + tempName + ' -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ' + filePath,
-								function(error, stdout, stderr) {
-									if(error) {
-										console.log('executing ffmpeg error 2');
-										cp.exec(
-										'/tmp/ffmpeg -analyzeduration 10000000 -probesize 100000000 -i /tmp/' + tempName + ' -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 ' + filePath,
-										function(error, stdout, stderr) {
-											if(error) {
-												console.log('executing ffmpeg error 3');
-											} else {
-												uploadS3(tempName, fileName, filePath, callback);
-											}
-										});
-									} else {
-										uploadS3(tempName, fileName, filePath, callback);
-									}
-								});
-
-
-								/*
-								console.log('executing ffmpeg error : ' + error);
-								cp.exec('ls -al /tmp/; rm -rf /tmp/' + tempName + ' ' + filePath,
-									function(error, stdout, stderr) {
-										if(error)
-											console.log('deleting temp error');
-										else
-											console.log('deleting temp ok');
-										console.log(stdout + stderr);
-										callback(null, 'error');
-									}
-								);
-								*/
-							} else {
-								console.log('executing ffmpeg');
-								uploadS3(tempName, fileName, filePath, callback);
-							}
-						}
-					);
+					convertMP3(tempName, fileName, callback);
 				});
 				req.on('error', callback);
 				req.write(data);
