11b076066a1412bd7bc32c3dbf50964471e4b2f0
hoony <leesh9267@snu.ac.kr>
2016-10-29 02:46:58 +0900

    make kpop_name dynamoDB table and put data about songId or artistId and its s3 url

--- lambda/ArtistBasedRecommendation/ArtistBasedRecommandation.js
+++ lambda/ArtistBasedRecommendation/ArtistBasedRecommandation.js
@@ -7,9 +7,9 @@
 const aws = require('aws-sdk');
 const lambda = new aws.Lambda();
 var s3 = new aws.S3();
+var dynamodb = new aws.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-    var dynamodb = new aws.DynamoDB.DocumentClient();
     var params = {
         TableName: 'kpop_chart',
         Key : {
@@ -39,7 +39,9 @@ exports.handler = (event, context, callback) => {
                     if(idx != -1){
                     songs[0].artistName = temp.substring(0,idx-1);
                     }
-                    var artistFileName = JSON.stringify(songs[0].artistId) + '1.mp3';
+
+                    var artistId = songs[0].artistId;
+                    var artistFileName = JSON.stringify(artistId) + '1.mp3';
                     var payload = {
                         text: songs[0].artistName,
                         fileName: artistFileName
@@ -48,24 +50,25 @@ exports.handler = (event, context, callback) => {
                         FunctionName: 'TTS',
                         Payload: JSON.stringify(payload)
                     };
-                    makeAudioFile(params, artistFileName);
+                    makeAudioFile(params, artistId, artistFileName);
                     for(var i=0; i<songs.length; i++){
                         temp = songs[i].songName;
                         idx = temp.indexOf("(");
                         if(idx != -1){
                             songs[i].songName = temp.substring(0,idx-1);
                         }
-                        var songFileName = songs[i].songId + '1.mp3';
+                        var songId = songs[i].songId;
+                        var songFileName = songId + '1.mp3';
                         payload = {
                             text: songs[i].songName,
                             fileName: songFileName
                         };
-                        console.log('fileName: ' + songFileName);
                         params = {
                             FunctionName: 'TTS',
                             Payload: JSON.stringify(payload)
                         };
-                        makeAudioFile(params, songFileName);
+                        songId = Number(songId);
+                        makeAudioFile(params, songId, songFileName);
                     }
                     callback(null, songs);
                 }
@@ -81,11 +84,25 @@ function tts(params){
     });
 }
 
-function makeAudioFile(params, filename){
-    var url = s3.getSignedUrl('getObject', {Bucket: 'koreantts', Key: filename});
-    if(url === null){
-        tts(params);
-    } else {
-        console.log(filename + ' already exists');
-    }
+function makeAudioFile(params, id, filename){
+    /*s3.getObject({Bucket: 'koreantts', Key: filename}, function (err, data) {
+            if(err) tts(params);
+            else console.log(filename + ' already exists');
+    });*/
+    dynamodb.get({TableName: 'kpop_name', Key: {"Id": id}}, function(err, data) {
+       if(err || Object.keys(data).length === 0){
+           tts(params);
+           dynamodb.put({
+               TableName: 'kpop_name',
+               Item: {
+                   "Id" : id,
+                   "url": "https://s3.amazonaws.com/koreantts/" + filename
+               }
+           }, function(err, data) {
+              if(err) console.log(err, err.stack);
+           });
+       } else {
+           console.log(id, 'url : ' + data.Item.url);
+       }
+    });
 }
