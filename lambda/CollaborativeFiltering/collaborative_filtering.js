9b19bfc01217dccef5db1b7f440a3259db1a8e9b
zuun77 <year2002777@gmail.com>
2016-12-06 01:05:55 +0900

    collaborative filtering recommend song from the user's playlist whose most played song is same with current user's

--- lambda/CollaborativeFiltering/collaborative_filtering.js
+++ lambda/CollaborativeFiltering/collaborative_filtering.js
@@ -6,15 +6,32 @@ var lambda = new aws.Lambda({
 
 const db = new aws.DynamoDB.DocumentClient();
 const S3_PREFIX = 'https://s3.amazonaws.com/kpopmusic/';
+const DEFAULT_SONG = 3929469;
+
+
+function getMostPlayedSongInPlaylist(songs) {
+	var songCount = {};
+	songs.forEach(function(song, idx) {
+		if(songCount[song]) songCount[song] += 1;
+		else songCount[song] = 1;
+	});
+	var mostPlayedSong, maxCount = 0;
+	Object.keys(songCount).forEach(function(key, idx) {
+		if(maxCount < songCount[key]) {
+			maxCount = songCount[key];
+			mostPlayedSong = key;
+		}
+	})
+	if(!mostPlayedSong) mostPlayedSong = DEFAULT_SONG;
+	return mostPlayedSong;
+}
 
 exports.handler = function(event, context, callback) {
 	var userId = event.userId;
-
-	var songId = 3929469; // default songs
+	var songId = DEFAULT_SONG; // default songs
 	async.waterfall([
 		function(cb) {
 			var playlistType = Math.floor(Math.random()*2); // 2 recommendations
-			playlistType = 0;
 			if(playlistType === 0) { // random user's playlist
 				db.scan({ 
 					TableName : 'kpop_playlist'
@@ -22,24 +39,55 @@ exports.handler = function(event, context, callback) {
 					if(err) {
 						cb(null); // default song
 					}
-					console.log(data);
 					var playlists = data.Items;
 					var random_idx = Math.floor(Math.random()*playlists.length);
 					var playlist = playlists[random_idx];
-					console.log(playlist);
 					if(playlist && playlist.songs) { // 
 						var songs = playlist.songs;
-						console.log(songs);
 						cb(null, songs);
 					} else {
 					    cb(null); // default song
 					}
 				});
 			} else { // TODO :similar playlist 
-			    cb(null);
+				db.scan({ 
+					TableName : 'kpop_playlist'
+				}, function(err, data) {
+					if(err) {
+						cb(null); // default song
+					}
+					var playlists = data.Items;
+					var playlist;
+					var mostPlayedSong = DEFAULT_SONG;
+					// get current user's most played song
+					playlists.forEach(function(p, idx) {
+						if(p.userId === userId) { 
+							mostPlayedSong = getMostPlayedSongInPlaylist(p.songs);
+							delete playlists[idx];
+						}
+					});
+
+					var recommendUserIdx = 0;
+					// find user whose most played song issame as current user's
+					playlists.forEach(function(p, idx) {
+						var userMostPlayed = getMostPlayedSongInPlaylist(p.songs);
+						if(userMostPlayed === mostPlayedSong) recommendUserIdx = idx;
+					});
+					var playlist = playlists[recommendUserIdx];
+					if(playlist && playlist.songs) { // 
+						var songs = playlist.songs;
+
+						// remove most played song from the list, to prevent recommendation is same all the time
+						songs.forEach(function(song, idx) {
+							if(song === mostPlayedSong) delete songs[idx];
+						});
+						cb(null, songs);
+					} else {
+					    cb(null); // default song
+					}
+				});
 			}
 		}, function(songs, cb) {
-		    console.log(songs);
 			if(songs) {
 				var type = Math.floor(Math.random() * 3);
 				if(type === 0) { // most recent played
@@ -48,19 +96,7 @@ exports.handler = function(event, context, callback) {
 				    var random_idx = Math.floor(Math.random()*songs.legnth);
 					songId = songs[random_idx];
 				} else { // most played
-					var songCount = {};
-					songs.forEach(function(song, idx) {
-						if(songCount[song]) songCount[song] += 1;
-						else songCount[song] = 1;
-					});
-					var maxSong, maxCount = 0;
-					Object.keys(songCount).forEach(function(key, idx) {
-						if(maxCount < songCount[key]) {
-							maxCount = songCount[key];
-							maxSong = key;
-						}
-					})
-					songId = maxSong;
+					songId = getMostPlayedSongInPlaylist(songs);
 				}
 				cb(null);
 			} else {
@@ -77,3 +113,4 @@ exports.handler = function(event, context, callback) {
 }
 
 //exports.handler();
+
