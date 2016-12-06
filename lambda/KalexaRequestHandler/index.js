f20f31fd828306c39c6e603fb1cce25bf5cc518b
yeongjinc <yeongjinc@gmail.com>
2016-12-06 18:28:25 +0900

    discard confusing utterances

--- lambda/KalexaRequestHandler/index.js
+++ lambda/KalexaRequestHandler/index.js
@@ -499,6 +499,11 @@ Kalexa.prototype.intentHandlers = {
 	"QuizAnswerIntent" : function(intent, session, response) {
 		var userId = session.user.userId;
 		var answer = intent.slots.Answer.value;
+		if(answer.toLowerCase() == 'true')
+			answer = 'True';
+		else
+			answer = 'False';
+
 		var payload = {
 			"userId" : userId,
 			"answer" : answer
